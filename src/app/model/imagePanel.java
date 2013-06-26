ed4da5c50c5b1b631c5ee4e841c0c1cc32681416
Patryk Kaczmarek <patryk.k@samsung.com>
2013-06-26 11:42:44 +0200

    missinig files

---
+++ src/app/model/imagePanel.java
@@ -0,0 +1,45 @@
+package app.model;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class imagePanel extends JLabel{
+	
+	public imagePanel() {
+		Dimension minimumSize = new Dimension(100, 50);
+	    setMinimumSize(minimumSize);
+	
+	}
+    
+	
+	public BufferedImage scaleImage(BufferedImage img, int width, int height, Color background) {
+	    int imgWidth = img.getWidth();
+	    int imgHeight = img.getHeight();
+	    if (imgWidth*height < imgHeight*width) {
+	        width = imgWidth*height/imgHeight;
+	    } else {
+	        height = imgHeight*width/imgWidth;
+	    }
+	    BufferedImage newImage = new BufferedImage(width, height,
+	            BufferedImage.TYPE_INT_RGB);
+	    Graphics2D g = newImage.createGraphics();
+	    try {
+	        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+	                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+	        g.setBackground(background);
+	        g.clearRect(0, 0, width, height);
+	        g.drawImage(img, 0, 0, width, height, null);
+	    } finally {
+	        g.dispose();
+	    }
+	    return newImage;
+	}
+}
+
+
